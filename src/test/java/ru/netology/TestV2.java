7fdd246c16f96318efb31a8f46192a9e5475bc86
Maksim Shalaev <maksimshalaev11@gmail.com>
2023-07-16 00:12:35 +0300

    Added test classes

---
+++ src/test/java/ru/netology/TestV2.java
@@ -0,0 +1,25 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class TestV2 {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .header("Content-Type", "application/json; charset=utf-8")
+                .contentType(ContentType.JSON)
+        ;
+    }
+}
---
+++ src/test/java/ru/netology/TestV3.java
@@ -0,0 +1,26 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TestV3 {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data")
+
+                .when()
+                .post("/post")
+
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
